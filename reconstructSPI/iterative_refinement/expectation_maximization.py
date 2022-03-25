d381e3cd38a6ade75fcbaa254c06fe47cbc1e0f8
thisTyler <t368@heim.ca>
2022-03-24 23:56:38 -0700

    fix to numpy array initialization

--- reconstructSPI/iterative_refinement/expectation_maximization.py
+++ reconstructSPI/iterative_refinement/expectation_maximization.py
@@ -431,9 +431,9 @@ class IterativeRefinement:
             Shape (n_pix, n_pix, n_pix)
         """
         shape = xyz.shape[0]
-        count_3d = np.ones(n_pix, n_pix, n_pix)
+        count_3d = np.ones((n_pix, n_pix, n_pix))
         count_3d[0,0,0] *= shape
-        inserted_slice_3d = np.ones(n_pix, n_pix, n_pix)
+        inserted_slice_3d = np.ones((n_pix, n_pix, n_pix))
         return inserted_slice_3d, count_3d
 
     @staticmethod
