9d2277d3fb1a1b6897b5afd065a2833948a677cb
Geoffrey Woollard <geoffwoollard@gmail.com>
2022-03-31 14:38:32 -0700

    precommit

--- reconstructSPI/iterative_refinement/expectation_maximization.py
+++ reconstructSPI/iterative_refinement/expectation_maximization.py
@@ -203,7 +203,7 @@ class IterativeRefinement:
             Shape (n_pix, n_pix, n_pix)
             map normalized by counts.
         """
-        return map_3d * counts / (norm_const + counts**2)
+        return map_3d * counts / (norm_const + counts ** 2)
 
     @staticmethod
     def apply_noise_model(map_3d_f_norm_1, map_3d_f_norm_2):
@@ -351,7 +351,7 @@ class IterativeRefinement:
         map_3d_f = np.ones_like(map_3d_f)
         xyz_rotated = np.ones_like(xy_plane)
 
-        size = n_rotations * n_pix**2
+        size = n_rotations * n_pix ** 2
         slices = np.random.normal(size=size)
         slices = slices.reshape((n_rotations, n_pix, n_pix))
         return slices, xyz_rotated
@@ -373,28 +373,52 @@ class IterativeRefinement:
         return projection_f_conv_ctf
 
     @staticmethod
-    def compute_bayesian_weights(particle, slices):
+    def compute_bayesian_weights(particle, slices, sigma):
         """Compute Bayesian weights of particle to slice.
 
-                Use Gaussian white noise model.
+        Assumes a Gaussian white noise model.
 
         Parameters
         ----------
-        particle : arr
-            Shape (n_pix, n_pix)
-
+        particle : complex64 arr
+            Shape (n_pix,n_pix)
         slices : complex64 arr
             Shape (n_slices, n_pix, n_pix)
+        sigma : float
+          Gaussian white noise std
 
         Returns
         -------
         bayesian_weights : float64 arr
             Shape (n_slices,)
+        z_norm_const : float64
+          Normalizaing constant.
+
+        Notes
+        -----
+        Follows closely Nelson (2021)
+        z_norm_const is $U_i$ in Eq. 8.18
+        em_loss is $l_i$ from $L = Sigma l_i$ in Eqs. 8.10, 8.21
+        bayesian_weights are $gamma_i$ in Eq 8.11
+        offset_safe is $K_i$ in Eqs. 8.17, 8.21
+
+        The expectation step is the calculation of bayesian_weights and z_norm_const
+        The maximization step is the insertion of the particles.
+          into the volume, corresponding to the rotations of the slices
+          weighted by bayesian_weights.
         """
-        n_slices = slices.shape[0]
-        particle = np.ones_like(particle)
-        bayes_factors = np.random.normal(size=n_slices)
-        return bayes_factors
+        corr_slices_particle = (
+            (particle[None, :, :] * slices.conj()).sum(axis=(1, 2)).real
+        )
+        slices_norm = np.linalg.norm(slices, axis=(1, 2)) ** 2
+        particle_norm = np.linalg.norm(particle) ** 2
+        scale = -((2 * sigma ** 2) ** -1)
+        log_bayesian_weights = scale * (slices_norm - 2 * corr_slices_particle)
+        offset_safe = log_bayesian_weights.max()
+        bayesian_weights = np.exp(log_bayesian_weights - offset_safe)
+        z_norm_const = 1 / bayesian_weights.sum()
+        em_loss = -np.log(z_norm_const) + offset_safe + scale * particle_norm
+        return bayesian_weights, z_norm_const, em_loss
 
     @staticmethod
     def apply_wiener_filter(projection, ctf, small_number):
@@ -531,49 +555,3 @@ class IterativeRefinement:
             Shape (n_pix, n_pix, n_pix)
         """
         return np.zeros(fft_array.shape)
-
-
-    @staticmethod
-    def compute_bayesian_weights(particle, slices, sigma):
-    """Compute Bayesian weights of particle to slice.
-            Assumes a Gaussian white noise model.
-            
-    Parameters
-    ----------
-    particle : complex64 arr
-        Shape (n_pix,n_pix)
-    slices : complex64 arr
-        Shape (n_slices, n_pix, n_pix)
-    sigma : float
-      Gaussian white noise std
-
-    Returns
-    -------
-    bayesian_weights : float64 arr
-        Shape (n_slices,)
-    z_norm_const : float64
-      Normalizaing constant. 
-      
-    Notes
-    -------
-    Follows closely Nelson (2021)
-    z_norm_const is $U_i$ in Eq. 8.18
-    em_loss is $l_i$ from $L = \Sigma l_i$ in Eqs. 8.10, 8.21
-    bayesian_weights are $\gamma_i$ in Eq 8.11
-    offset_safe is $K_i$ in Eqs. 8.17, 8.21
-
-    The expectation step is the calculation of bayesian_weights and z_norm_const
-    The maximization step is the insertion of the particles 
-      into the volume, corresponding to the rotations of the slices
-      weighted by bayesian_weights 
-    """
-    corr_slices_particle = (particle[None,:,:]*slices.conj()).sum(axis=(1,2)).real
-    slices_norm = np.linalg.norm(slices,axis=(1,2))**2
-    particle_norm = np.linalg.norm(particle)**2
-    scale = -(2*sigma**2)**-1
-    log_bayesian_weights = scale*(slices_norm - 2*corr_slices_particle)
-    offset_safe = log_bayesian_weights.max()
-    bayesian_weights = np.exp(log_bayesian_weights-offset_safe)
-    z_norm_const = 1 / bayesian_weights.sum()
-    em_loss = -np.log(z_norm_const) + offset_safe + scale*particle_norm
-    return bayesian_weights, z_norm_const, em_loss
