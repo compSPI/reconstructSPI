Fixed rotation ambiguity. Added and explained tests. Better handling of xy planes

reconstructSPI/iterative_refinement/expectation_maximization.py
@@ -340,10 +340,12 @@ class IterativeRefinement:
     def generate_slices(map_3d_f, xy_plane, n_pix, rots):
         """Generate slice coordinates by rotating xy plane.
 
-                Interpolate values from map_3d_f onto 3D coordinates.
+        Interpolate values from map_3d_f onto 3D coordinates.
 
-        See how scipy map_values used to interpolate in
-        https://github.com/geoffwoollard/compSPI/blob/stash_simulate/src/simulate.py#L111
+                
+        Shift the space into a centered position before rotating and 
+        revert shift after rotation. This preserves the bounds of the 
+        space. 
 
         Parameters
         ----------
@@ -370,10 +372,9 @@ class IterativeRefinement:
         """
         n_rotations = rots.shape[0]
         slices = np.empty((n_rotations, map_3d_f.shape[0], map_3d_f.shape[1]))
-        xy_planes = np.repeat(np.expand_dims(xy_plane, axis=0), n_rotations, axis=0)
+        xy_planes = np.empty((n_rotations, n_pix**2, 3))
         for i in range(n_rotations):
-            for xy in range(xy_plane.shape[1]):
-                xy_planes[i, :, xy] = rots[i] @ (xy_planes[i, :, xy])
+            xy_planes[i] = rots[i] @ (xy_plane + 0.5) - 0.5
 
             slices[i] = map_coordinates(map_3d_f, xy_planes[i] + n_pix // 2).reshape(
                 (n_pix, n_pix)

tests/test_expectation_maximization.py
@@ -120,32 +120,56 @@ def test_generate_slices(test_ir, n_particles, n_pix):
     assert slices.shape == (n_particles, n_pix, n_pix)
     assert xyz_rotated_planes.shape == (n_particles, 3, n_pix**2)
 
-    rot_test_map = np.array(
-        [
-            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
-            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
-            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
-        ]
-    )
+    # ---------------------------------
+    # 90-degree rotation test.
+    # Map has ones in central xz-plane. 
+    # Rotating by -90 degrees about y
+    # should produce a slice of ones.
+    rot_test_map = np.zeros((4, 4, 4))
+    rot_test_map[2] = np.ones((4, 4))
 
     rot_mat = np.array(
         [
-            [[0, 0, 1], [0, 1, 0], [-1, 0, 0]],
+            [[0, 0, -1], [0, 1, 0], [1, 0, 0]],
         ]
     )
 
-    xy_plane = np.array(
-        [
-            [-1, 0, 1, -1, 0, 1, -1, 0, 1],
-            [-1, -1, -1, 0, 0, 0, 1, 1, 1],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ]
-    )
+    xy_plane = np.array([
+        [ -2, -1,  0,  1, -2, -1,  0,  1, -2, -1, 0, 1, -2, -1, 0, 1 ],
+        [ -2, -2, -2, -2, -1, -1, -1, -1,  0,  0, 0, 0,  1,  1, 1, 1 ],
+        [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0, 0,  0,  0, 0, 0 ]
+    ])
 
     slices, xyz_rotated_planes = test_ir.generate_slices(
         rot_test_map, xy_plane, 3, rot_mat
     )
     assert np.allclose(slices[0], np.ones_like(slices[0]))
+    # ---------------------------------
+
+    # ---------------------------------
+    # 180-degree rotation test.
+    # Map has ones in central xz-plane. 
+    # Rotating by 180 degrees about z
+    # should produce a similar matrix,
+    # namely a slice of ones in the -1-line.
+    # Inherits same xy_plane from above. 
+    rot_test_map = np.zeros((4, 4, 4))
+    rot_test_map[2] = np.ones((4, 4))
+
+    rot_mat = np.array(
+        [
+            [[-1, 0, 0], [0, -1, 0], [0, 0, 1]],
+        ]
+    )
+
+    expected_slice = np.zeros((4, 4))
+    expected_slice[:,2] = 1
+
+    slices, xyz_rotated_planes = test_ir.generate_slices(
+        rot_test_map, xy_plane, 3, rot_mat
+    )
+    assert np.allclose(slices[0], expected_slice)
+    # ---------------------------------
 
 
 def test_apply_ctf_to_slice(test_ir, n_pix):