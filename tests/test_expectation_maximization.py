47ca31ddb48e5309d8551a835be207018e6ac2e6
Geoffrey Woollard <geoffwoollard@gmail.com>
2022-04-04 10:24:03 -0700

    remove +/- 0.5

--- tests/test_expectation_maximization.py
+++ tests/test_expectation_maximization.py
@@ -90,7 +90,7 @@ def test_grid_SO3_uniform(test_ir, n_particles):
 def test_generate_xy_plane(test_ir, n_pix):
     """Test generation of xy plane."""
     xy_plane = test_ir.generate_xy_plane(n_pix)
-    assert xy_plane.shape == (3, n_pix**2)
+    assert xy_plane.shape == (3, n_pix ** 2)
 
     n_pix_2 = 2
     plane_2 = np.array([[-1, 0, -1, 0], [-1, -1, 0, 0], [0, 0, 0, 0]])
@@ -122,7 +122,7 @@ def test_generate_slices(test_ir, n_particles, n_pix):
     slices, xyz_rotated_planes = test_ir.generate_slices(map_3d, xy_plane, n_pix, rots)
 
     assert slices.shape == (n_particles, n_pix, n_pix)
-    assert xyz_rotated_planes.shape == (n_particles, 3, n_pix**2)
+    assert xyz_rotated_planes.shape == (n_particles, 3, n_pix ** 2)
 
     map_plane_ones = np.zeros((n_pix, n_pix, n_pix))
     map_plane_ones[n_pix // 2] = np.ones((n_pix, n_pix))
@@ -136,7 +136,11 @@ def test_generate_slices(test_ir, n_particles, n_pix):
     slices, xyz_rotated_planes = test_ir.generate_slices(
         map_plane_ones, xy_plane, n_pix, rot_90deg_about_y
     )
-    assert np.allclose(slices[0], np.ones_like(slices[0]))
+    all_ones = np.ones_like(slices[0])
+    map_coordinates_artefact = 0
+    all_ones[0, :] = map_coordinates_artefact
+    all_ones[:, 0] = map_coordinates_artefact
+    assert np.allclose(slices[0], all_ones)
 
     rot_180deg_about_z = np.array(
         [
@@ -146,6 +150,9 @@ def test_generate_slices(test_ir, n_particles, n_pix):
 
     expected_slice = np.zeros((n_pix, n_pix))
     expected_slice[:, n_pix // 2 - 1] = 1
+
+    expected_slice[0, :] = map_coordinates_artefact
+    expected_slice[:, 0] = map_coordinates_artefact
 
     slices, xyz_rotated_planes = test_ir.generate_slices(
         map_plane_ones, xy_plane, n_pix, rot_180deg_about_z
